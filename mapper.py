66826b9bda27d3b772479c3ea6dff1bc98fa9c7b
Dario Vazquez <elveon97@localhost.localdomain>
2019-09-14 13:23:32 -0600

    Changed files path

---
+++ mapper.py
@@ -0,0 +1,15 @@
+"""mapper.py"""
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output)
+        # tab-delimited words with default count 1
+        print '%s\t%s' % (word, 1)
